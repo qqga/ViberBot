e4c135db2ede09ce8628e01d875d0ff7db4cc156
qqga <qqga@mail.ru>
2019-10-09 08:47:50 +0500

    WebApiClient

--- ViberBot.Test/TestClient.cs
+++ ViberBot.Test/TestClient.cs
@@ -1,6 +1,9 @@
 using NUnit.Framework;
+using System;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
+using ViberBot.Model.Request;
 
 namespace ViberBot.Test
 {
@@ -20,5 +23,29 @@ namespace ViberBot.Test
 
             string result = await setWebhook.Content.ReadAsStringAsync();
         }
+
+        [Test]
+        [Ignore("Not for test")]
+        public async Task SendMsg()
+        {
+            ViberBotClient viberBotClient = new ViberBotClient("");
+            HttpResponseMessage setWebhook = await viberBotClient.SendMessage(new TextMessage("test  log event reloaded 3", "TestProject", ""));
+
+            string result = await setWebhook.Content.ReadAsStringAsync();
+            viberBotClient.Dispose();
+            TestContext.WriteLine(result);
+        }
+
+        [Test]
+        [Ignore("Not for test")]
+        public async Task SendMsgWebApi()
+        {
+            ViberWebApiNotifyer viberWebApiNotifyer = new ViberWebApiNotifyer("https://", "", "TestProjectWebApi", "");
+            Task<HttpResponseMessage> sendMsg = viberWebApiNotifyer.SendMsg("test event");
+            HttpResponseMessage httpResponseMessage = await sendMsg;
+            string result = await httpResponseMessage.Content.ReadAsStringAsync();
+            TestContext.WriteLine(result);
+        }
+
     }
 }
---
+++ ViberBot/ViberBotWebApiClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViberBot
+{
+    public class ViberWebApiNotifyer : IDisposable
+    {
+        readonly string _Token;
+        public Encoding Encoding { get; set; } = Encoding.UTF8;
+        public string Url { get; set; }
+        HttpClient _httpClient = new HttpClient();
+        public string From { get; set; }
+        public string To { get; set; }
+        public ViberWebApiNotifyer(string url, string token, string from, string to)
+        {
+            To = to;
+            From = from;
+            _Token = token;
+            Url = url;
+        }
+        public Task<HttpResponseMessage> SendMsg(string msg, string from = null, string to = null)
+        {
+            MultipartFormDataContent byteArrayContent = new MultipartFormDataContent();
+            byteArrayContent.Add(StringToHttpContent(from ?? From), "from");
+            byteArrayContent.Add(StringToHttpContent(to ?? To), "to");
+            byteArrayContent.Add(StringToHttpContent(msg), "msg");
+            byteArrayContent.Add(StringToHttpContent(_Token), "token");
+
+            return _httpClient.PostAsync(Url, byteArrayContent);
+        }
+
+        HttpContent StringToHttpContent(string str)
+        {
+            byte[] bytes = this.Encoding.GetBytes(str);
+            ByteArrayContent byteArrayContent = new ByteArrayContent(bytes);
+            return byteArrayContent;
+        }
+
+        public void Dispose()
+        {
+            _httpClient.Dispose();
+        }
+    }
+}
